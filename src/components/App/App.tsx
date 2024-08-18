aafa2821ca65324ee80afbcf295f0c9f39826443
Pooya Tolideh <pooyatolideh@Pooyas-Laptop.local>
2024-08-18 00:26:44 -0400

    implement FizzBuzzInput and FizzBuzzCard in App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,32 @@
-import { ReactNode, useState } from "react";
+import { useState } from "react";
+import FizzBuzzInput from "../FizzBuzzInput";
+import FizzBuzzCard from "../FizzBuzzCard";
+
+type NumberEntryType = {
+  id: string;
+  num: number;
+};
 
 function App() {
+  const [entries, setEntries] = useState<NumberEntryType[]>([]);
+  const handleSubmitNumber = (num: number) => {
+    const nextEntry: NumberEntryType = { id: crypto.randomUUID(), num };
+    setEntries([nextEntry, ...entries]);
+  };
+  console.log(entries);
+
   return (
-    <main className="container mx-auto">
-      <header className="flex flex-col items-center justify-center p-4 border-b-2 border-slate-100">
+    <div className="container flex flex-col mx-auto h-screen border-2 border-red-300">
+      <header className="flex flex-shrink-0 items-center justify-center p-4 border-b-2 border-slate-100">
         <h1 className="mx-4 font-bold">FizzBuzz</h1>
       </header>
-    </main>
+      <main className="flex-grow overflow-scroll">
+        {entries.map((item) => {
+          return <FizzBuzzCard key={item.id} num={item.num} />;
+        })}
+      </main>
+      <FizzBuzzInput handleSubmitNumber={handleSubmitNumber} />
+    </div>
   );
 }
 
